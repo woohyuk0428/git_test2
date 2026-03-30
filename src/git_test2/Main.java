66349b807d139087e2567429b571e2759204f389
woohyuk0428 <whkang03@gmail.com>
2026-03-30 21:26:17 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,8 +2,9 @@ package git_test2;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.say();
-	}
+    public static void main(String[] args) {
+        Hello h = new Hello("hello world");
+        h.say();
+    }
+
 }
